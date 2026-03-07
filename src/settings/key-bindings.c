90fa67dc62eac2fa683fb26d02d46b8dce3f78de
陈湛明 <chenzhanming04@foxmail.com>
2026-03-07 11:35:53 +0000

    Fix type

--- src/settings/key-bindings.c
+++ src/settings/key-bindings.c
@@ -56,7 +56,7 @@ void KeyBindings_Render(const FXT_Config *config)
 	dsubimage(0, 12, &Img_Settings_KeyBindings_Caption, 0, 50 * config->Language, 128, 50, 0);
 
 	const char *keyString = KeyCode_ToString(config->PhysicalKeyOfFxTapKey[selectedKeyBinding]);
-	assert(0 <= selectedKeyBinding && selectedKeyBinding < FXT_MaxKeyCount);
+	assert(FXT_Key_K1 <= selectedKeyBinding && selectedKeyBinding <= FXT_Key_S1);
 
 	drect_border(92, 26, 106, 36, C_WHITE, 1, C_BLACK);
 	dtext(94, 28, C_BLACK, FXT_Key_ToString(selectedKeyBinding));
@@ -66,7 +66,7 @@ void KeyBindings_Render(const FXT_Config *config)
 
 void KeyBindings_AcceptEvent(const key_event_t e, FXT_Config *config)
 {
-	assert(0 <= selectedKeyBinding && selectedKeyBinding < FXT_MaxKeyCount);
+	assert(0 <= selectedKeyBinding && selectedKeyBinding <= FXT_Key_S1);
 
 	switch (e.key)
 	{
