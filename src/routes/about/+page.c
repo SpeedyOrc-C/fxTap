9ae9cdf1cb3c79914385a9c824b7712c0b64687e
陈湛明 <chenzhanming04@foxmail.com>
2026-03-26 09:59:26 +0000

    Reformat

--- src/routes/about/+page.c
+++ src/routes/about/+page.c
@@ -3,10 +3,9 @@
 #include "fxconv-assets.h"
 #include "ui.h"
 
-static constexpr auto PageCount = 3;
-
 void UI_About()
 {
+	static constexpr auto PageCount = 3;
 	int selectedPage = 0;
 
 	while (true)
@@ -25,12 +24,12 @@ void UI_About()
 
 		case 2:
 			dsubimage(0, 0, &Img_About, 0, 128, 128, 64, 0);
-
 			dtext(1, 12, C_BLACK, "GCC Version:");
 			dprint(1, 12 + 1 + dfont_default()->line_height, C_BLACK,__VERSION__);
 			dtext(1, 12 + 2 * (1 + dfont_default()->line_height), C_BLACK, "Compiled at:");
 			dprint(1, 12 + 3 * (1 + dfont_default()->line_height), C_BLACK, "%s %s", __DATE__, __TIME__);
 			break;
+
 		default:
 			assert(false && "Invalid about page");
 		}
@@ -45,12 +44,14 @@ void UI_About()
 			return;
 		case KEY_UP:
 		case KEY_LEFT:
-			if (selectedPage > 0) selectedPage -= 1;
+			if (selectedPage > 0)
+				selectedPage -= 1;
 			break;
 		case KEY_DOWN:
 		case KEY_RIGHT:
 		case KEY_EXE:
-			if (selectedPage < PageCount - 1) selectedPage += 1;
+			if (selectedPage < PageCount - 1)
+				selectedPage += 1;
 			break;
 		default:
 			break;
