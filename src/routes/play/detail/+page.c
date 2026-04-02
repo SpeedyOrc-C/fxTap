75d5baeacdd7ef38a40717d35b041ffab7046fa0
陈湛明 <chenzhanming04@foxmail.com>
2026-04-02 09:31:00 +0100

    Simplify beatmap detail page

--- src/routes/play/detail/+page.c
+++ src/routes/play/detail/+page.c
@@ -1,52 +1,26 @@
+#include <stdio.h>
 #include <fxTap/database.h>
+#include <gint/display.h>
 #include <gint/keyboard.h>
-#include "assets.h"
+#include "ui.h"
 
 void UI_Play_Detail(const FXT_DatabaseRecord *record)
 {
-	// TODO)) Allow word wrapping in strings
 	dclear(C_WHITE);
 
-	dprint(0, 0, C_BLACK, "[TITLE]");
-	dprint(0, 8, C_BLACK, "%s", record->Title);
+	dprint(0, 0, C_BLACK, "%s", record->Title);
+	dprint(0, 8, C_BLACK, "%s", record->Artist);
+	dprint(0, 24, C_BLACK, "%uK  OD:%.1f", record->ColumnCount, record->OverallDifficulty);
+	dprint(0, 40, C_BLACK, "Note count:");
 
-	dprint(0, 16, C_BLACK, "[ARTIST]");
-	dprint(0, 24, C_BLACK, "%s", record->Artist);
-
-	dprint(0, 32, C_BLACK, "[KEY] %u   [OD] %.2f", record->ColumnCount, record->OverallDifficulty);
-
-	dprint(0, 40, C_BLACK, "[COLUMN SIZES]");
-	switch (record->ColumnCount)
 	{
-	case 4:
-		dprint(0, 48, C_BLACK, "%d %d %d %d",
-		       record->ColumnSize[0],
-		       record->ColumnSize[1],
-		       record->ColumnSize[2],
-		       record->ColumnSize[3]
-		);
-		break;
-	case 5:
-		dprint(0, 48, C_BLACK, "%d %d %d %d %d",
-		       record->ColumnSize[0],
-		       record->ColumnSize[1],
-		       record->ColumnSize[2],
-		       record->ColumnSize[3],
-		       record->ColumnSize[4]
-		);
-		break;
-	case 6:
-		dprint(0, 48, C_BLACK, "%d %d %d %d %d %d",
-		       record->ColumnSize[0],
-		       record->ColumnSize[1],
-		       record->ColumnSize[2],
-		       record->ColumnSize[3],
-		       record->ColumnSize[4],
-		       record->ColumnSize[5]
-		);
-		break;
-	default:
-		break;
+		char buffer[48] = {};
+		char *p = buffer;
+
+		for (size_t i = 0; i < record->ColumnCount; i += 1)
+			p += sprintf(p, i == 0 ? "%d" : ",%d", record->ColumnSize[i]);
+
+		dtext(0, 48, C_BLACK, buffer);
 	}
 
 	dupdate();
