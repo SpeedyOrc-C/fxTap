a289b104e9ba8098cdb60134ea3bd0023482a6e1
陈湛明 <chenzhanming04@foxmail.com>
2026-04-13 16:55:51 +0100

    Support mods: Full Combo & Perfect

--- src/routes/play/mods/+page.c
+++ src/routes/play/mods/+page.c
@@ -2,6 +2,13 @@
 #include "assets.h"
 #include "ui.h"
 
+static void Invert1(void) { drect(2, 13, 62, 27, C_INVERT); }
+static void Invert2(void) { drect(2, 30, 62, 44, C_INVERT); }
+static void Invert3(void) { drect(2, 47, 62, 61, C_INVERT); }
+static void Invert4(void) { drect(65, 13, 125, 27, C_INVERT); }
+static void Invert5(void) { drect(65, 30, 125, 44, C_INVERT); }
+static void Invert6(void) { drect(65, 47, 125, 61, C_INVERT); }
+
 void UI_Play_Mods(FXT_ModOption *option, const FXT_Config *config)
 {
 	static size_t page = 0;
@@ -20,30 +27,18 @@ void UI_Play_Mods(FXT_ModOption *option, const FXT_Config *config)
 		switch (page)
 		{
 		case 0:
-			if (option->Mirror)
-				drect(2, 13, 62, 27, C_INVERT);
-
-			if (option->Hidden)
-				drect(2, 30, 62, 44, C_INVERT);
-
-			if (option->FadeIn)
-				drect(2, 47, 62, 61, C_INVERT);
-
-			if (option->Random)
-				drect(65, 13, 125, 27, C_INVERT);
-
-			if (option->NoRelease)
-				drect(65, 30, 125, 44, C_INVERT);
-
-			if (option->HoldOff)
-				drect(65, 47, 125, 61, C_INVERT);
-
+			if (option->Mirror) Invert1();
+			if (option->Hidden) Invert2();
+			if (option->FadeIn) Invert3();
+			if (option->Random) Invert4();
+			if (option->NoRelease) Invert5();
+			if (option->HoldOff) Invert6();
 			break;
 
 		case 1:
-			if (option->Reverse)
-				drect(2, 13, 62, 27, C_INVERT);
-
+			if (option->Reverse) Invert1();
+			if (option->FullCombo) Invert4();
+			if (option->Perfect) Invert5();
 			break;
 
 		default:
@@ -107,8 +102,19 @@ void UI_Play_Mods(FXT_ModOption *option, const FXT_Config *config)
 			case KEY_F1:
 				option->Reverse = ! option->Reverse;
 				break;
-			default:
+			case KEY_4:
+			case KEY_F4:
+				option->FullCombo = ! option->FullCombo;
+				if (option->FullCombo)
+					option->Perfect = false;
 				break;
+			case KEY_5:
+			case KEY_F5:
+				option->Perfect = ! option->Perfect;
+				if (option->Perfect)
+					option->FullCombo = false;
+				break;
+			default: break;
 			}
 
 		default:
